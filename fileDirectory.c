5141f1c6f42a8eba3b48c377505d2e924eefc84b
Juan Navarro Jr <48901040+MenPeko@users.noreply.github.com>
2025-10-14 20:03:20 -0700

    In-Class(10-14-25)

---
+++ fileDirectory.c
@@ -0,0 +1,19 @@
+/*
+* Group: The Polarizers
+* Group Members: Dani Luna, Praveen Sinha, Emily Perez, Juan Navarro
+*/
+typedef struct {
+char name[256]; // file name
+int size; //file size represented by int
+long timestampCreated; // Timestamp
+int userID; // User ID
+int permissions; // 0,1,2,3 kernal, user ect..
+} dir
+entry;
+_
+int main() {
+dir
+entry file;
+_
+return 0;
+}
